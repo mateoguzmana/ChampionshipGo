dd611274519fb2d1bc40166ba627f0659819cc1e
vente16 <jw2@misena.edu.co>
2017-05-01 18:08:24 -0500

    I changed the page 'Login', add an image and organize function 'login' on Login.js

--- api/models/connection.class.php
+++ api/models/connection.class.php
@@ -6,12 +6,27 @@
 
         public function __construct(){
 
-           $this->connection = new PDO("mysql:host=localhost;dbname=championshipgo;charset=utf8","root","");
-           
+           $this->connection = new PDO("mysql:host=localhost;dbname=datos;charset=utf8","root","");
+
+           if($this->connection){
+
+               echo "Conexión extablecida";
+
+           }else{
+
+                echo "No se estableció la conexión";
+           }
+
            return $this->connection;
             
         }
 
+
     } 
+
+    $conexion = new connection();
+    
+    
+
 
 ?>
--- templates/js/Login.js
+++ templates/js/Login.js
@@ -1,19 +1,29 @@
 $(function () {
 
+     /*this function get value of the inputs and the controller validate if exist the email and
+       password on the database.
+     */
     function Login(){
       
       $('#Login').click(function(){
 
+        
+
         email    =  $('#Email').val();
         password =  $('#Password').val();
         
-         var data = {"email":email, "password":password, "method":post};
+        var data = {"email":email, 
+                    "password":password, 
+
+                    };
 
           $.post('../api/controllers/persons.controller.php', data, function(validar){
 
             alert(validar);
 
           }); 
+
+
 
 
         });
